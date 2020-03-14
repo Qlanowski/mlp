480e00fc1754f43f160134cd9cad4e0980b19e2c
qbazilla <qbazilla@hotmail.com>
2020-03-14 17:58:28 +0100

    move init of weights to separate method

--- train/mlp.py
+++ train/mlp.py
@@ -1,17 +1,24 @@
 import numpy as np
+import pandas as pd
 
 
 class MLP:
 
     def __init__(self, network_size, is_bias, activation_function):
-        self.weights = [
-            np.ones(y, x)
-            for x, y
-            in zip(network_size[:-1], network_size[1:])
-        ]
-        self.biases = None if not is_bias else [
-            np.ones(y, 1)
-            for y
-            in network_size[1:]
-        ]
+        self.network_size = network_size
+        self.is_bias = is_bias
         self.activation_function = activation_function
+
+    def __init_weights__(self, seed=None):
+        np.random.seed(seed)
+        self.weights = [
+            np.random.randn(y, x)
+            for x, y
+            in zip(self.network_size[:-1], self.network_size[1:])
+        ]
+        self.biases = None if not self.is_bias else [
+            np.random.randn(y, 1)
+            for y
+            in self.network_size[1:]
+        ]
+

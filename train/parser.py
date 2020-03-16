5071331acbae65c9c4309d4e6ca710014fabe46c
qbazilla <qbazilla@hotmail.com>
2020-03-16 13:21:34 +0100

    implement func for merging y classes into single column

--- train/parser.py
+++ train/parser.py
@@ -6,8 +6,16 @@ def split_y_classes(y):
     classes = y.iloc[:, 0].unique()
     result = pd.DataFrame(
         np.zeros((len(y.index), len(classes))),
-        columns=["cls%s" % cls for cls in classes],
+        columns=[cls for cls in classes],
     )
     for col, cls in zip(result.columns, classes):
         result[col] = [int(val == cls) for val in y.iloc[:, 0]]
-    return result
+    return result, classes
+
+
+def merge_y_classes(y, classes):
+    y_to_np = y.to_numpy()
+    return pd.DataFrame(
+        classes[np.argmax(row)]
+        for row in y_to_np
+    )

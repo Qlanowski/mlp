3e1dc4e1d7d9697a1672c518600402616d2261d0
qbazilla <qbazilla@hotmail.com>
2020-03-17 21:18:18 +0100

    update tester

--- tests/tester.py
+++ tests/tester.py
@@ -1,16 +1,17 @@
 import tests.generator as generator
-from train.functions.sigmoid import Sigmoid
+import  train.visualization.dataset_visualization as dv
+from train.functions.relu import ReLU
 from train.cost_functions import QuadraticCostFunction
 
 config = generator.TestConfig(
     train_file="../classification/data.three_gauss.train.100.csv",
     test_file="../classification/data.three_gauss.test.100.csv",
-    hid_layers=[3, 5, 7],
-    act_func=Sigmoid(),
+    hid_layers=[10, 10, 10],
+    act_func=ReLU(),
     cost_func=QuadraticCostFunction(),
     is_bias=True,
     batch_size=10,
-    iterations=1000,
+    iterations=None,
     lr=0.3,
     moment=0.2,
     seed=123456789
@@ -18,7 +19,10 @@ config = generator.TestConfig(
 
 fields = ['iterations']
 
-values = [[value] for value in range(100, 5000, 500)]
+values = [[value] for value in range(50, 1001, 50)]
+
+# dv.visualise_data_set_classes(config.train_file, save=True, filename='train_data.png')
+# dv.visualise_data_set_classes(config.test_file, save=True, filename='test_data.png')
 
 generator.save_classification_plot_to_accuracy(
     config=config,
@@ -27,5 +31,5 @@ generator.save_classification_plot_to_accuracy(
     x_label='iterations',
     y_label='Accuracy [%]',
     title='Classification Accuracy',
-    filename='acc_to_iter_classification.png'
+    filename='relu.png'
 )

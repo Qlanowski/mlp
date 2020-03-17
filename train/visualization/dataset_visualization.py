e4b4c9524868446bbc77dd6a759336a055fb6694
qbazilla <qbazilla@hotmail.com>
2020-03-17 19:42:53 +0100

    add func for visualising datasets

---
+++ train/visualization/dataset_visualization.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+
+colors = ["red", "blue", "green", "yellow", "purple", "orange", "white", "black"]
+
+
+def visualise_data_set_classes(x, y):
+    classes = y.iloc[:, 0].unique()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    global colors
+    for cls, col in zip(classes, colors):
+        x_cls = x[y.iloc[:, 0] == cls]
+        sp = ax.scatter(
+            x_cls.iloc[:, 0],
+            x_cls.iloc[:, 1],
+            alpha=0.5,
+            c=col,
+            edgecolors='none',
+            s=50,
+            label="class %d" % cls
+        )
+    ax.set_xlabel("x1")
+    ax.set_ylabel("x2")
+    ax.set_title("Dataset visualisation")
+    ax.legend(loc=2)
+    fig.show()

ef2c7d06923f0a9ea48ea890ec80ec381bd20ab4
qbazilla <qbazilla@hotmail.com>
2020-03-18 13:05:12 +0100

    refactor test plots generator

--- tests/generator.py
+++ tests/generator.py
@@ -10,7 +10,7 @@ from train.functions.sigmoid import Sigmoid
 
 
 class TestConfig:
-    def __init__(self, train_file, test_file, hid_layers, act_func, cost_func, is_bias, batch_size, iterations, lr, moment, seed):
+    def __init__(self, train_file, test_file, hid_layers, act_func, cost_func, is_bias, batch_size, lr, moment, seed):
         self.train_file = train_file
         self.test_file = test_file
         self.hidden_layers = hid_layers
@@ -18,63 +18,84 @@ class TestConfig:
         self.cost_function = cost_func
         self.is_bias = is_bias
         self.batch_size = batch_size
-        self.iterations = iterations
         self.learning_rate = lr
         self.momentum = moment
         self.seed = seed
 
 
-def load_data(filename):
-    df = pd.read_csv(filename)
-    x = df.iloc[:, :-1]
-    y = df.iloc[:, -1:]
-    return x, y
+class TestResult:
+    def __init__(self, iterations, test_set_result, test_set_score, train_set_result, train_set_score):
+        self.iterations = iterations
+        self.test_set_result = test_set_result
+        self.test_set_score = test_set_score
+        self.train_set_result = train_set_result
+        self.train_set_score = train_set_score
 
 
-def get_single_classification_test_result(config):
-    x, y = load_data(config.train_file)
-    y, classes = pr.split_y_classes(y)
-    layers = [len(x.columns)] + config.hidden_layers + [len(y.columns)]
-    mlp = MLP(
+def get_test_mlp(config, input_size, output_size, activation_functions):
+    layers = [input_size] + config.hidden_layers + [output_size]
+    return MLP(
         network_size=layers,
         is_bias=config.is_bias,
-        activation_functions=[config.activation_function] * (len(layers) - 2) + [Sigmoid()],
+        activation_functions=activation_functions,
         cost_function=config.cost_function,
         visualizer=Visualizer(None, None)
     )
+
+
+def get_single_classification_test_result(config, iterations):
+    x_train, y = pr.load_data(config.train_file)
+    y_train, classes = pr.split_y_classes(y)
+    activation_functions = [config.activation_function] * len(config.hidden_layers) + [Sigmoid()]
+    mlp = get_test_mlp(config, len(x_train.columns), len(y_train.columns), activation_functions)
     mlp.train(
-        x,
-        y,
-        iterations=config.iterations,
+        x_train,
+        y_train,
+        iterations=iterations,
         batch_size=config.batch_size,
         learning_rate=config.learning_rate,
         momentum=config.momentum,
         seed=config.seed
     )
-    x_test, y_test = load_data(config.test_file)
-    result = pr.merge_y_classes(mlp.predict(x_test), classes)
-    accuracy = acc.get_classification_accuracy(result, y_test)
-    print('accuracy:', accuracy)
-    return result, accuracy
+    x_test, y_test = pr.load_data(config.test_file)
+    test_set_result = pr.merge_y_classes(mlp.predict(x_test), classes)
+    train_set_result = pr.merge_y_classes(mlp.predict(x_train), classes)
+    test_set_accuracy = acc.get_classification_accuracy(test_set_result, y_test)
+    train_set_accuracy = acc.get_classification_accuracy(train_set_result, y)
+    print('test set accuracy:', test_set_accuracy)
+    print('train set accuracy:', train_set_accuracy)
+    return TestResult(
+        iterations,
+        test_set_result,
+        test_set_accuracy,
+        train_set_result,
+        train_set_accuracy
+    )
 
 
-def get_classification_test_series_result(config, fields, values):
+def get_classification_test_series_result(config, iterations_list):
     results = []
-    for val_set in values:
-        for field, val in zip(fields, val_set):
-            setattr(config, field, val)
-        results.append(get_single_classification_test_result(config))
+    for iterations in iterations_list:
+        results.append(get_single_classification_test_result(config, iterations))
     return results
 
 
-def save_classification_plot_to_accuracy(config, fields, values, x_label, y_label, title, filename):
-    results = get_classification_test_series_result(config, fields, values)
-    accuracies = [a * 100 for res, a in results]
+def visualise_classification_test_series(results, title, save=False, filename=None):
+    train_accuracies = [res.train_set_score * 100 for res in results]
+    test_accuracies = [res.test_set_score * 100 for res in results]
+    iterations = [res.iterations for res in results]
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_ylim([30, 100])
-    p = ax.plot(values, accuracies, 'b')
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    ax.set_ylim(0, 100)
+    ax.set_yticks(range(0, 100, 5))
+    ax.grid(True)
+    train_p = ax.plot(iterations, train_accuracies, 'r', label='training data')
+    test_p = ax.plot(iterations, test_accuracies, 'b', label='test data')
+    ax.set_xlabel('Number of iterations')
+    ax.set_ylabel('Accuracy [%]')
     ax.set_title(title)
-    fig.savefig(filename)
+    ax.legend(loc='lower right')
+    if save:
+        fig.savefig(filename)
+    else:
+        fig.show()
--- tests/tester.py
+++ tests/tester.py
@@ -1,35 +1,30 @@
 import tests.generator as generator
-import  train.visualization.dataset_visualization as dv
+import train.visualization.dataset_visualization as dv
 from train.functions.relu import ReLU
 from train.cost_functions import QuadraticCostFunction
 
 config = generator.TestConfig(
     train_file="../classification/data.three_gauss.train.100.csv",
     test_file="../classification/data.three_gauss.test.100.csv",
-    hid_layers=[10, 10, 10],
+    hid_layers=[5, 5],
     act_func=ReLU(),
     cost_func=QuadraticCostFunction(),
     is_bias=True,
     batch_size=10,
-    iterations=None,
     lr=0.3,
     moment=0.2,
     seed=123456789
 )
 
-fields = ['iterations']
-
-values = [[value] for value in range(50, 1001, 50)]
-
 # dv.visualise_data_set_classes(config.train_file, save=True, filename='train_data.png')
 # dv.visualise_data_set_classes(config.test_file, save=True, filename='test_data.png')
 
-generator.save_classification_plot_to_accuracy(
-    config=config,
-    fields=fields,
-    values=values,
-    x_label='iterations',
-    y_label='Accuracy [%]',
-    title='Classification Accuracy',
-    filename='relu.png'
+
+results = generator.get_classification_test_series_result(config, range(0, 601, 10))
+
+generator.visualise_classification_test_series(
+    results=results,
+    title='Classification Accuracy for ReLU',
+    save=True,
+    filename='results/test.png'
 )

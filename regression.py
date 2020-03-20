1fc1eead5fe85637c6514ed193f7b526e63c393c
Karol Ulanowski <ulanowskikarol@gmail.com>
2020-03-21 00:37:31 +0100

    working regression.py

--- regression.py
+++ regression.py
@@ -1,40 +1,47 @@
-import mnist_loader
 from train import network
 import train.parser as pr
 import numpy as np
 
+from train.functions.identity import Identity
+from train.functions.relu import ReLU
+from train.functions.sigmoid import Sigmoid
 
-def load_classification(filename):
+
+def load_regression(filename):
     x, y = pr.load_data(filename)
-    y = pr.split_y_classes(y)
-
     x = np.array(x)
-    y = np.array(y[0])
+    y = np.array(y)
     return [(np.array(_x).reshape(-1, 1), np.array(_y).reshape(-1, 1)) for _x, _y in zip(x, y)]
 
 
-def load_test_classification(filename):
+def load_test_regression(filename):
     x, y = pr.load_data(filename)
     x = np.array(x)
     y = np.array(y)
     return [(np.array(_x).reshape(-1, 1), _y[0]) for _x, _y in zip(x, y)]
 
 
-train_filename = r'C:\Users\ulano\PycharmProjects\mlp\classification\data.three_gauss.test.100.csv'
-test_filename = r'C:\Users\ulano\PycharmProjects\mlp\classification\data.three_gauss.test.100.csv'
+train_filename = r'C:\Users\ulano\PycharmProjects\mlp\regression\data.activation.train.100.csv'
+test_filename = r'C:\Users\ulano\PycharmProjects\mlp\regression\data.activation.test.100.csv'
 
-train_data = load_classification(train_filename)
-test_data = load_test_classification(test_filename)
+train_data = load_regression(train_filename)
+test_data = load_test_regression(test_filename)
 
-layers = [train_data[0][0].shape[0], 10, 10, train_data[0][1].shape[0]]
-epochs = 100
+layers = [train_data[0][0].shape[0],7, train_data[0][1].shape[0]]
+epochs = 300
 batch_size = 10
-learning_rate = 0.1
+learning_rate = 0.0001
+activation_functions = [ReLU()] * (len(layers) - 2) + [Identity()]
 
-net = network.Network(layers)
-net.SGD(train_data, epochs=epochs, , 0.1)
+net = network.Network(layers, activation_functions)
+net.SGD(train_data, epochs=epochs, mini_batch_size=batch_size, eta=learning_rate)
 
-test_results = [(np.argmax(net.feedforward(x))+1, y) for (x, y) in test_data]
-result = sum(int(x == y) for (x, y) in test_results)
-print(result / len(test_results))
-net.evaluate(test_data)
+test_results = [(net.feedforward(x), y) for (x, y) in test_data]
+npa = np.array(test_results)
+result = sum((x - y)**2 for (x, y) in test_results)
+print(f'Test score: {result}')
+
+train_results = [(net.feedforward(x), y) for (x, y) in train_data]
+result2 = sum((x - y)**2 for (x, y) in train_results)
+print(f'Train score: {result2}')
+
--- train/network.py
+++ train/network.py
@@ -140,14 +140,3 @@ class Network(object):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
         return (output_activations - y)
-
-
-#### Miscellaneous functions
-def sigmoid(z):
-    """The sigmoid function."""
-    return 1.0 / (1.0 + np.exp(-z))
-
-
-def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
-    return sigmoid(z) * (1 - sigmoid(z))

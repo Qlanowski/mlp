2c1ef0954bdc6fb677dc41ffb8ec8abc0220bdde
Karol Ulanowski <ulanowskikarol@gmail.com>
2020-03-21 15:39:49 +0100

    saving kaggle

--- new_kaggle_digit_prediction.py
+++ new_kaggle_digit_prediction.py
@@ -1,7 +1,7 @@
-from train import network
-import train.parser as pr
+from train.network import Network
 import numpy as np
 import pandas as pd
+import os
 
 from train.cost_functions import QuadraticCostFunction
 from train.functions.sigmoid import Sigmoid
@@ -18,7 +18,7 @@ def train_test(filename, train_frac, seed):
 
 
 def prepare_to_train(df):
-    x = df.iloc[:, 1:]/255
+    x = df.iloc[:, 1:] / 255
     y = df.iloc[:, 0]
     y_vec = np.array([np.zeros(10) for i in y])
     for v, i in zip(y_vec, y):
@@ -29,41 +29,66 @@ def prepare_to_train(df):
 
 
 def prepare_to_test(df):
-    x = df.iloc[:, 1:]/255
+    x = df.iloc[:, 1:] / 255
     y = df.iloc[:, 0]
     x = np.array(x)
     y = np.array(y)
     return [(np.array(_x).reshape(-1, 1), _y) for _x, _y in zip(x, y)]
 
 
+def load_submission_test_data(filename):
+    df = pd.read_csv(filename)
+    x = np.array(df / 255)
+    return [np.array(_x).reshape(-1, 1) for _x in x]
+
+
+def save_submission(filename, network):
+    submission_test_data = load_submission_test_data(filename)
+    result_y = [[inx + 1, int(np.argmax(network.predict(x)))] for inx, x in enumerate(submission_test_data)]
+    submission = pd.DataFrame(result_y, columns=['ImageId', 'Label'])
+
+    results_directory = 'kaggle_results/'
+    if not os.path.exists(results_directory):
+        os.makedirs(results_directory)
+
+    submission.to_csv(results_directory + 'submission_1.csv', index=False)
+
+
 def run_scoring(iteration, iterations, network, train_data, test_data):
     if iteration % 500:
         return
 
-    test_results = [(np.argmax(network.predict(x)), y) for (x, y) in test_data]
-    test_result = sum(int(x == y) for (x, y) in test_results)
-    test_acc = test_result / len(test_results)
+    if len(test_data):
+        test_results = [(np.argmax(network.predict(x)), y) for (x, y) in test_data]
+        test_result = sum(int(x == y) for (x, y) in test_results)
+        test_acc = test_result / len(test_results)
+    else:
+        test_acc = -1
 
-    train_results = [(np.argmax(network.predict(x)), y) for (x, y) in train_data]
-    train_result = sum(int(x == y) for (x, y) in train_results)
-    train_acc = train_result / len(train_results)
+    if len(train_data):
+        train_results = [(np.argmax(network.predict(x)), y) for (x, y) in train_data]
+        train_result = sum(int(x == y) for (x, y) in train_results)
+        train_acc = train_result / len(train_results)
+    else:
+        train_acc = -1
 
     print(f'Iteration {iteration}/{iterations} completed; Test acc: {test_acc}; Train acc: {train_acc}')
 
 
 def main():
     seed = 12345
+    train_fraction = 0.7
     train_set_filename = r'kaggle_digits/train.csv'
     test_set_filename = r'kaggle_digits/test.csv'
 
-    train_df, test_df = train_test(train_set_filename, 0.7, seed)
+    train_df, test_df = train_test(train_set_filename, train_fraction, seed)
     train_data = prepare_to_train(train_df)
     test_data = prepare_to_test(test_df)
     train_data_evaluation = prepare_to_test(train_df)
 
     layers = [train_data[0][0].shape[0], 10, train_data[0][1].shape[0]]
     batch_size = 10
-    iterations = len(train_data) / batch_size * 10
+    iterations = len(train_data) / batch_size * 2
     learning_rate = 3
     activation_functions = [Sigmoid()] * (len(layers) - 2) + [Sigmoid()]
     cost_function = QuadraticCostFunction()
@@ -72,11 +97,11 @@ def main():
     momentum = 0.01
     visualizer = Visualizer(layers, is_bias)
 
-    net = network.Network(layers,
-                          is_bias=is_bias,
-                          activation_functions=activation_functions,
-                          cost_function=cost_function,
-                          visualizer=visualizer)
+    net = Network(layers,
+                  is_bias=is_bias,
+                  activation_functions=activation_functions,
+                  cost_function=cost_function,
+                  visualizer=visualizer)
     net.train(train_data,
               iterations=iterations,
               mini_batch_size=batch_size,
@@ -86,6 +111,8 @@ def main():
               iteration_function=run_scoring,
               iteration_train_data=train_data_evaluation,
               iteration_test_data=test_data)
+
+    save_submission(test_set_filename, net)
 
 
 if __name__ == "__main__":
--- train/network.py
+++ train/network.py
@@ -1,22 +1,4 @@
-# %load network.py
-
-"""
-network.py
-~~~~~~~~~~
-IT WORKS
-
-A module to implement the stochastic gradient descent learning
-algorithm for a feedforward neural network.  Gradients are calculated
-using backpropagation.  Note that I have focused on making the code
-simple, easily readable, and easily modifiable.  It is not optimized,
-and omits many desirable features.
-"""
-
-#### Libraries
-# Standard library
 import random
-
-# Third-party libraries
 import numpy as np
 
 
